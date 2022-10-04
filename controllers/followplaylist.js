5dcb80c3c9f471769b91cc6581ddb073638de391
mrtnwh <lagonegromartin@gmail.com>
2022-10-04 20:27:03 -0300

    put follow playlist

---
+++ controllers/followplaylist.js
@@ -0,0 +1,31 @@
+const { request, response } = require("express");
+const axios = require("axios");
+
+async function putFollowPlaylist(req = request, res = response) {
+  const { id } = req.params;
+  try {
+    const response = await axios.put(
+      `https://api.spotify.com/v1/playlists/${id}/followers`,
+      {
+        public: false,
+      },
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.TOKEN}`,
+        },
+      }
+    );
+    res.status(200).json({
+      id: id,
+      msg: "se ha seguido a la playlist solicitada",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  putFollowPlaylist,
+};
